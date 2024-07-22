f770e635d04925c3bdeb81fd3f107af347f5c42c
Lucky Ivanius <luckzivanius@gmail.com>
2024-07-22 22:06:07 +0700

    fix main implementation on withdraw

--- src/index.js
+++ src/index.js
@@ -3,26 +3,23 @@ import { doWithdraw, prepare, USDT_JETTONS_INFO, wallet } from "./wallet.js";
 async function main() {
   const address = await wallet.getAddress();
 
+  console.log("Address:", address.toString(true, true, true));
+
   await prepare();
 
   const walletUsdtData = await USDT_JETTONS_INFO.jettonWallet.getData();
   const usdtBalance = walletUsdtData.balance;
 
-  console.log(
-    "Address:",
-    address.toString(true, true, true),
-    "- USDT Balance:",
-    usdtBalance.toString()
-  );
+  console.log("USDT Balance:", usdtBalance.toString());
 
-  const recipientAddress = "";
+  const recipientAddress = "YOUR_RECIPIENT_ADDRESS";
 
   const withdrawalRequest = {
     amount: walletUsdtData.balance,
     toAddress: recipientAddress,
   };
 
-  // await doWithdraw(withdrawalRequest);
+  await doWithdraw(withdrawalRequest);
 }
 
 main();
